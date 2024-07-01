6546873be681d01bc87cfdebb43b2c747969cf71
Prapti <121173640+praaptiiiii@users.noreply.github.com>
2024-07-01 22:56:12 +0530

    Create StudentLogin.jsx

---
+++ StudentLogin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+
+function StudentLogin() {
+  return (
+    <div className="student-login">
+      <LoginForm title="Student" />
+    </div>
+  );
+}
+
+export default StudentLogin;
