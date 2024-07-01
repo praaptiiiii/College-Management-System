c4fb4ebe850087e9fde90867b9c7e9fe3b99ce18
Prapti <121173640+praaptiiiii@users.noreply.github.com>
2024-07-01 22:54:15 +0530

    Create LoginButtons.jsx

---
+++ LoginButtons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoginButtons() {
+  return (
+    <div className="login-buttons">
+      <button>Student Login</button>
+      <button>Teacher Login</button>
+      <button>Admin Login</button>
+    </div>
+  );
+}
+
+export default LoginButtons;
